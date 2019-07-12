3450fc4704606236ddef1d4d34f5acfb1ac90c11
hjzhu <347136225@qq.com>
2019-07-12 10:24:41 +0800

    Update image_crop_keep_ratio.py

--- image_crop_keep_ratio.py
+++ image_crop_keep_ratio.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Jul 11 16:36:27 2019
 
-@author: LL
+@author: hjzhu
 
 """
 import cv2,glob
@@ -33,18 +33,6 @@ if __name__ == "__main__":
             print("w=",kuang_gao_bi)
             img_cut = img_src[sx:int(height-sx), : ]
             img_cut = cv2.resize(img_cut,(240,320),interpolation=cv2.INTER_CUBIC)
-
-#        elif 4/3 > kuang_gao_bi >3/4 :         
-#            sx = int((height-(width*w))/2)
-#            print("w=",kuang_gao_bi)
-#            img_cut = img_src[sx:int(height-sx), : ]
-#            img_cut = cv2.resize(img_cut,(240,320),interpolation=cv2.INTER_CUBIC)
-            
-#        elif  3/4 < kuang_gao_bi < 1:         
-#            sx = int((height-(width*w))/2)
-#            print("w=",kuang_gao_bi)
-#            img_cut = img_src[sx:int(height-sx), : ]
-#            img_cut = cv2.resize(img_cut,(240,320),interpolation=cv2.INTER_CUBIC)
             
         else:                       # 刚好
              img_cut = cv2.resize(img_src,(240,320),interpolation=cv2.INTER_CUBIC)
